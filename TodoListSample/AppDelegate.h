a46f86bb05d46bdc28c9a419f0d473969e89b809
hiraya.shingo <hirayashingo@HL00065.local>
2014-08-18 19:06:47 +0900

    Initial Commit

---
+++ TodoListSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoListSample
+//
+//  Created by hiraya.shingo on 2014/08/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoListSample/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  TodoListSample
+//
+//  Created by hiraya.shingo on 2014/08/18.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
